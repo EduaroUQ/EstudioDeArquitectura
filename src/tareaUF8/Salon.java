7e1903abc29da77f2a30bd67f9e5c4c1391fd674
Hugo Rubio Crespo <hugo.rubio.crespo@gmail.com>
2025-02-19 10:30:00 +0100

    Salon2

--- src/tareaUF8/Salon.java
+++ src/tareaUF8/Salon.java
@@ -1,5 +1,22 @@
 package tareaUF8;
 
 public class Salon extends Estancia {
+	//atributos
+	private int tomasDeRed;
+	private double metrosCuadradosTerraza;
+	
+	//constructor
+	public Salon(String nombre, double metrosCuadrados, int puertas, int ventanas, int tomasDeRed,
+			double metrosCuadradosTerraza) {
+		super(nombre, metrosCuadrados, puertas, ventanas);
+		this.tomasDeRed = tomasDeRed;
+		this.metrosCuadradosTerraza = metrosCuadradosTerraza;
+	}
+	
+	public double getMetrosCuadradosTerraza() {
+		return super.getMetrosCuadrados()+metrosCuadradosTerraza;
+	}
+	
+	
 
 }
